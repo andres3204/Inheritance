b8613da6d5c1c40c79f861432270a945fbd3f10d
Andres Enrique Jiménez Mogollón <andresejimenez@hotmail.com>
2022-06-05 16:25:45 -0500

    Add project files.

---
+++ Inheritance/ItalianChef.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance
+{
+    internal class ItalianChef : Chef
+    {
+        public override void MakeSpecialDish()
+        {
+            Console.WriteLine(" The Chef makes a special Italian dish.");
+        }
+        public void MakeItalianDish()
+        {
+            Console.WriteLine(" The Italian Chef makes Pizza.");
+
+        }
+    }
+}
---
+++ Inheritance/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Inheritance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Chef chef1 = new Chef();
+            chef1.MakeSpecialDish();
+
+            ItalianChef chef2 = new ItalianChef();
+            chef2.MakeSpecialDish();
+            
+
+            Console.ReadLine();
+        }
+    }
+}
